4fb24b5f508d42ccbe741d14d54b7dce94c35f8b
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-01-25 21:50:55 -0500

    Initial Commit

---
+++ HomeDesignerAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeDesignerAR
+//
+//  Created by Dean Wahle on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeDesignerARUITests/HomeDesignerARUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeDesignerARUITestsLaunchTests.swift
+//  HomeDesignerARUITests
+//
+//  Created by Dean Wahle on 1/25/22.
+//
+
+import XCTest
+
+class HomeDesignerARUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
