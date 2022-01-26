5d4f5596e04127cda821188f054d08bf286b4d06
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-01-25 23:32:11 -0500

    User interface created using UICollectionView and included functions

--- HomeDesignerAR/ViewController.swift
+++ HomeDesignerAR/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, UICollectionViewDataSource , UICollectionViewDelegate{
 
-class ViewController: UIViewController {
-
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [ ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.itemsCollectionView.dataSource = self
+        self.itemsCollectionView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    //function that configures cells in the collection view
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //dequeueReusableCell returns a cell type based on the identifier
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! itemCell
+        //index path points to n'th item in the array, and assigns it the corresponding label
+        cell.itemLabel.text = self.itemsArray[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.green
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
     }
 
 
---
+++ HomeDesignerAR/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  HomeDesignerAR
+//
+//  Created by Dean Wahle on 1/25/22.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    @IBOutlet weak var itemLabel: UILabel!
+    
+}
